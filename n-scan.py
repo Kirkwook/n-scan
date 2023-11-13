f4df453714a6848b53da714e257498ba01ed5005
Tanner2020 <tanjhall2020@gmail.com>
2023-11-13 11:44:07 -0600

    Created function to get hostname from Ip addr.

--- n-scan.py
+++ n-scan.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import argparse
 
+# Retrieves IP address based on the provided hostname
 def get_IP(hostname):
     try:
         ip_address = socket.gethostbyname(hostname)
@@ -10,10 +11,21 @@ def get_IP(hostname):
         print(f"Error: {e}")
         sys.exit(1)
 
+# Retrieves hostname based on the provided IP address
+def get_hostname(ip_addr):
+    try:
+        hostname = socket.gethostbyaddr(ip_addr)
+        return hostname[0]
+    except socket.error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 def main():
-    print(get_IP("openvas.infosec.uwec.edu"))
+    ip_host = input("Enter the target hostname or IP address:\n")
+    print(get_IP(ip_host))
 
 
 if __name__ == "__main__":
     main()
+    #openvas.infosec.uwec.edu
+    #172.26.0.50

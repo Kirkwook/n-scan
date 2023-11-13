027b4c54399e7747e952daf81fbe782975ab3e2e
Kirkwook <benjain.d.kirkwood@gmail.com>
2023-11-13 10:54:45 -0600

    added DNS request method

---
+++ n-scan.py
@@ -0,0 +1,11 @@
+import socket
+import sys
+import argparse
+
+def get_IP(hostname):
+    try:
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except socket.error as e:
+        print(f"Error: {e}")
+        sys.exit(1)

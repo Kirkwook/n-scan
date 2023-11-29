6e03c43efc795b6987ee6af244f3b94d7e3b3a7b
Tanner2020 <tanjhall2020@gmail.com>
2023-11-29 08:16:02 -0600

    Created IO version of n-scan

---
+++ n-scan-io.py
@@ -0,0 +1,109 @@
+import socket
+import sys
+import argparse
+import ping3
+from tqdm import tqdm
+
+# Retrieves IP address based on the provided hostname
+def get_ip(hostname):
+    try:
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except socket.error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+# Retrieves hostname based on the provided IP address
+def get_hostname(ip_addr):
+    try:
+        hostname = socket.gethostbyaddr(ip_addr)
+        return hostname[0]
+    except socket.error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+#  Checks input and passes the output to the correct retrieval method
+def get_host_ip(ip_host):
+    if(ip_host[0].isalpha() == True):
+        ip = get_ip(ip_host)
+        hostname = ip_host
+    else:
+        hostname = get_hostname(ip_host)
+        ip = ip_host
+    result = [hostname, ip]
+    return result
+
+def scan_ports(ip_address, ports):
+    open_ports = []
+    for port in tqdm(ports):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.05)
+        result = sock.connect_ex((ip_address, port))
+        if result == 0:
+            open_ports.append(port)
+        sock.close()
+    return open_ports
+
+def get_service_name(port):
+    try:
+        service_name = socket.getservbyport(port)
+        return service_name
+    except socket.error:
+        return "Unknown"
+    
+def ping_host(host):
+    try:
+        ping = ping3.ping(host, timeout=2)  # Set timeout value (in seconds)
+        if ping is not None:
+            return ping
+        else:
+            return -1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def main():
+    line_count = 0
+    file_path = input("Enter the filepath of the source file:\n")
+    file_object = open(file_path, "r")
+    file_output = open("output.txt", "w")
+
+    file_output.write("N-SCAN NETWORK SCANNER - JOB SOURCE FILE: " + file_path + "\n")
+    for lines in file_object:
+        file_output.write("\n")
+        file_output.write("----------------------------------------------------------\n")
+        line_count += 1
+        file_output.write("Scanning for line " + str(line_count) + " content: " + lines.strip() + "\n")
+        print("Network scan of: " + lines.strip())
+
+        ip_host = lines.strip()
+        result = get_host_ip(ip_host)
+        file_output.write("Hostname: " + result[0] + "\n")
+        file_output.write("IP Address: " + result[1] + "\n")
+
+        ping_time = ping_host(result[1])
+        if ping_time >= 0:
+            file_output.write("Ping response time: " + str(ping_time) + " ms\n")
+        else:
+            file_output.write("Ping response time was unsuccessful.\n")
+
+        ports_to_scan = range(1, 1025)
+        open_ports = scan_ports(result[1], ports_to_scan)
+        file_output.write("Open ports found: " + str(len(open_ports)) + "\n")
+
+        ports_list = []
+        if open_ports:
+            for port in open_ports:
+                service_name = get_service_name(port)
+                found_port = [port, service_name]
+                file_output.write("Port: " + str(found_port[0]) + ", Service: " + found_port[1] +"\n")
+                ports_list.append(found_port)
+        else:
+            print("No open ports found.")
+
+        file_output.write("----------------------------------------------------------\n")
+    
+    file_object.close()
+    file_output.close()
+
+if __name__ == "__main__":
+    main()
